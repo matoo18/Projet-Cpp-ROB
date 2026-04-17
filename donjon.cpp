8ad44495f39e49f666149bfa531444c3a7dd4453
Paul CAMUS <paul.camus@etu.sorbonne-universite.fr>
2026-04-17 16:26:00 +0200

    Création module en charge de la génération du donjon.

---
+++ donjon.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cmath>
+#include"case.cpp" // faut créer un fichier .h je crois mais ça à l'air d'être une galère...
+
+using namespace std;
+
+class Donjon {
+    public:
+        void initialiserGrille(int largeur, int hauteur){};
+};
+
